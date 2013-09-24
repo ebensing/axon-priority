38e1ee6745b0b7703765adbbc82860f2800c7db9
EJ Bensing <ebensing@rm-dash-r.com>
2013-09-24 10:53:21 -0400

    added lots of support files; tests not passing yet

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ var exporting = {};
 
 exporting.plugin = require('./lib/priority');
 
+exporting.patchSocket = function (sock) {
+  sock.usePlugin = function (plugin) {
+    this.use(plugin({fallback : this.enqueue}));
+  }
+}
+
 module.exports = exports = exporting;
